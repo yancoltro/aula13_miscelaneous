3c48bdc5b74c393440b4c520aef053c8008eb965
Yan Coltro <yan.coltro@gmai.com>
2022-11-09 01:27:38 -0300

    chore: init with intent receiver

--- lib/services/intent_receiver_service.dart
+++ lib/services/intent_receiver_service.dart
@@ -1,6 +1,27 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart' hide Intent;
+import 'package:receive_intent/receive_intent.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uri_to_file/uri_to_file.dart';
+import 'package:share_plus/share_plus.dart';
+
 class IntentReceiverService{
 
-  checkForIntent(){
-    
+ Future<void> checkForIntent() async {
+    final receivedIntent = await ReceiveIntent.getInitialIntent();
+
+    if (!mounted) return;
+
+    Widget _resolved = await _decideShow(receivedIntent!);
+
+    setState(() {
+      // _initialIntent = receivedIntent!;
+      _toShow = _resolved;
+    });
   }
+
+ 
 }
